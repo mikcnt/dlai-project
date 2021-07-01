544d018324c870dffb0d948b0aaa3af987eb75f6
mikcnt <11929535+mikcnt@users.noreply.github.com>
2021-07-01 17:30:26 +0200

    Add scripts for generation of the rollouts, and dataset classes

---
+++ src/generate_data.py
@@ -0,0 +1,103 @@
+import argparse
+import os
+from os.path import join, exists
+import random
+
+import gym
+import numpy as np
+
+
+# def sample_continuous_policy(env, seq_len, dt):
+#     """Sample a continuous policy.
+#     Atm, action_space is supposed to be a box environment. The policy is
+#     sampled as a brownian motion a_{t+1} = a_t + sqrt(dt) N(0, 1).
+#     :args action_space: gym action space
+#     :args seq_len: number of actions returned
+#     :args dt: temporal discretization
+#     :returns: sequence of seq_len actions
+#     """
+#     actions = [env.action_space.sample()]
+#     print(env.action_space)
+#     exit()
+#     for _ in range(seq_len):
+#         daction_dt = np.random.randn()
+#         actions.append(
+#             np.clip(
+#                 actions[-1] + math.sqrt(dt) * daction_dt,
+#                 env.action_space.low,
+#                 env.action_space.high,
+#             )
+#         )
+#     return actions
+
+
+def generate_data(rollouts, data_dir, noise_type):  # pylint: disable=R0914
+    """Generates data"""
+    assert exists(data_dir), "The data directory does not exist..."
+
+    # env = gym.make("procgen:procgen-coinrun-v0")
+    env = gym.make("procgen:procgen-coinrun-v0")
+    seq_len = 1000
+
+    for i in range(rollouts):
+        os.makedirs(f"{data_dir}/rollout_{i}", exist_ok=True)
+        env.reset()
+        # env.env.viewer.window.dispatch_events()
+        if noise_type == "white":
+            a_rollout = [env.action_space.sample() for _ in range(seq_len)]
+        # elif noise_type == "brown":
+        #     a_rollout = sample_continuous_policy(env, seq_len, 1.0 / 50)
+
+        s_rollout = []
+        r_rollout = []
+        d_rollout = []
+
+        t = 0
+        while True:
+            action = a_rollout[t]
+
+            s, r, done, _ = env.step(action)
+            # save single move frame
+            np.savez(
+                join(data_dir, f"rollout_{i}/img_{t}"),
+                observations=np.array(s),
+                rewards=np.array(r),
+                actions=np.array(action),
+                terminals=np.array(done),
+            )
+            t += 1
+            # env.env.viewer.window.dispatch_events()
+            s_rollout += [s]
+            r_rollout += [r]
+            d_rollout += [done]
+            # save entire rollout
+            if done:
+                print("> End of rollout {}, {} frames...".format(i, len(s_rollout)))
+                np.savez(
+                    join(data_dir, f"rollout_{i}/rollout_{i}"),
+                    observations=np.array(s_rollout),
+                    rewards=np.array(r_rollout),
+                    actions=np.array(a_rollout),
+                    terminals=np.array(d_rollout),
+                )
+                break
+
+
+if __name__ == "__main__":
+    random.seed(42)
+    np.random.seed(42)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rollouts", type=int, help="Number of rollouts", default=100)
+    parser.add_argument(
+        "--dir", type=str, help="Where to place rollouts", default="../all_rollouts/"
+    )
+    parser.add_argument(
+        "--policy",
+        type=str,
+        choices=["white", "brown"],
+        help="Noise type used for action sampling.",
+        default="white",
+    )
+    args = parser.parse_args()
+    os.makedirs(args.dir, exist_ok=True)
+    generate_data(args.rollouts, args.dir, args.policy)
--- src/pl_data/dataset.py
+++ src/pl_data/dataset.py
@@ -1,6 +1,9 @@
+import glob
+import os
 from typing import Dict, Tuple, Union
 
 import hydra
+import numpy as np
 import omegaconf
 import pytorch_lightning as pl
 import torch
@@ -10,19 +13,75 @@ from torch.utils.data import Dataset
 from src.common.utils import PROJECT_ROOT
 
 
-class MyDataset(Dataset):
-    def __init__(self, name: ValueNode, path: ValueNode, **kwargs):
+class GameSceneDataset(Dataset):
+    def __init__(
+        self,
+        name: ValueNode,
+        path: ValueNode,
+    ):
         super().__init__()
         self.path = path
         self.name = name
+        self.fpaths = sorted(glob.glob(os.path.join(path, "rollout_*/img_*.npz")))
+        self.indices = np.arange(0, len(self.fpaths))
+        # n_trainset = int(len(indices) * (1.0 - test_ratio))
+        # n_trainset = len(indices)
+        # self.train_indices = indices[:n_trainset]
+        # self.test_indices = indices[n_trainset:]
+        # self.indices = self.train_indices if training else self.test_indices
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, idx):
+        npz = np.load(self.fpaths[self.indices[idx]])
+        obs = torch.as_tensor(npz["observations"]).permute(2, 0, 1)
+        return obs
+
+    def __repr__(self) -> str:
+        return f"MyDataset({self.name=}, {self.path=})"
+
+
+class GameEpisodeDataset(Dataset):
+    def __init__(
+        self,
+        name: ValueNode,
+        path: ValueNode,
+        seq_len: int = 32,
+        # training: bool = True,
+        # test_ratio: float = 0.01,
+    ):
+        super().__init__()
+        self.path = path
+        self.name = name
+        self.fpaths = sorted(glob.glob(os.path.join(path, "rollout_*/rollout_*.npz")))
+        self.indices = np.arange(0, len(self.fpaths))
+        # n_trainset = int(len(indices) * (1.0 - test_ratio))
+        # self.train_indices = indices[:n_trainset]
+        # self.test_indices = indices[n_trainset:]
+        # self.indices = self.train_indices if training else self.test_indices
+        self.seq_len = seq_len
 
     def __len__(self) -> int:
-        raise NotImplementedError
+        return len(self.indices)
 
-    def __getitem__(
-        self, index
-    ) -> Union[Dict[str, torch.Tensor], Tuple[torch.Tensor, torch.Tensor]]:
-        raise NotImplementedError
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+        npz = np.load(self.fpaths[self.indices[idx]])
+        obs = npz["observations"]  # (rollout_dimension, H, W, C) np array
+        roll_dim, H, W, C = obs.shape
+        actions = npz["actions"][:roll_dim]  # (rollout_dimension, ) np array
+        n_seq = roll_dim // self.seq_len
+        end_seq = n_seq * self.seq_len  # T' = end of sequence
+
+        obs = obs[:end_seq].reshape(
+            [-1, self.seq_len, H, W, C]
+        )  # (N_seq, seq_len, H, W, C)
+        actions = actions[:end_seq].reshape([-1, self.seq_len])  # (N_seq, seq_len)
+
+        obs = torch.as_tensor(obs).permute(0, 1, 4, 2, 3)
+        actions = torch.as_tensor(actions, dtype=torch.float)
+
+        return {"observations": obs, "actions": actions}
 
     def __repr__(self) -> str:
         return f"MyDataset({self.name=}, {self.path=})"
@@ -30,7 +89,7 @@ class MyDataset(Dataset):
 
 @hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="default")
 def main(cfg: omegaconf.DictConfig):
-    dataset: MyDataset = hydra.utils.instantiate(
+    dataset: GameEpisodeDataset = hydra.utils.instantiate(
         cfg.data.datamodule.datasets.train, _recursive_=False
     )
 
