df25a26b6d3c46f9fa776076f50c9dbef3ebb159
mikcnt <11929535+mikcnt@users.noreply.github.com>
2021-08-24 17:15:19 +0200

    Update  script to work with Plumber, ignoring backgrounds and using restricted themes

--- src/generate_data.py
+++ src/generate_data.py
@@ -7,46 +7,22 @@ import gym
 import numpy as np
 
 
-# def sample_continuous_policy(env, seq_len, dt):
-#     """Sample a continuous policy.
-#     Atm, action_space is supposed to be a box environment. The policy is
-#     sampled as a brownian motion a_{t+1} = a_t + sqrt(dt) N(0, 1).
-#     :args action_space: gym action space
-#     :args seq_len: number of actions returned
-#     :args dt: temporal discretization
-#     :returns: sequence of seq_len actions
-#     """
-#     actions = [env.action_space.sample()]
-#     print(env.action_space)
-#     exit()
-#     for _ in range(seq_len):
-#         daction_dt = np.random.randn()
-#         actions.append(
-#             np.clip(
-#                 actions[-1] + math.sqrt(dt) * daction_dt,
-#                 env.action_space.low,
-#                 env.action_space.high,
-#             )
-#         )
-#     return actions
-
-
-def generate_data(rollouts, data_dir, noise_type):  # pylint: disable=R0914
+def generate_data(rollouts, data_dir):
     """Generates data"""
-    assert exists(data_dir), "The data directory does not exist..."
 
-    # env = gym.make("procgen:procgen-coinrun-v0")
-    env = gym.make("procgen:procgen-coinrun-v0")
+    env = gym.make(
+        "procgen:procgen-plunder-v0",
+        use_backgrounds=False,
+        restrict_themes=True,
+        use_monochrome_assets=True,
+    )
     seq_len = 1000
 
     for i in range(rollouts):
         os.makedirs(f"{data_dir}/rollout_{i}", exist_ok=True)
         env.reset()
-        # env.env.viewer.window.dispatch_events()
-        if noise_type == "white":
-            a_rollout = [env.action_space.sample() for _ in range(seq_len)]
-        # elif noise_type == "brown":
-        #     a_rollout = sample_continuous_policy(env, seq_len, 1.0 / 50)
+
+        a_rollout = [env.action_space.sample() for _ in range(seq_len)]
 
         s_rollout = []
         r_rollout = []
@@ -66,7 +42,6 @@ def generate_data(rollouts, data_dir, noise_type):  # pylint: disable=R0914
                 terminals=np.array(done),
             )
             t += 1
-            # env.env.viewer.window.dispatch_events()
             s_rollout += [s]
             r_rollout += [r]
             d_rollout += [done]
@@ -91,13 +66,6 @@ if __name__ == "__main__":
     parser.add_argument(
         "--dir", type=str, help="Where to place rollouts", default="../all_rollouts/"
     )
-    parser.add_argument(
-        "--policy",
-        type=str,
-        choices=["white", "brown"],
-        help="Noise type used for action sampling.",
-        default="white",
-    )
     args = parser.parse_args()
     os.makedirs(args.dir, exist_ok=True)
-    generate_data(args.rollouts, args.dir, args.policy)
+    generate_data(args.rollouts, args.dir)
