d9b8f08518cec28d4a4203e3ca8b4871b7eaed83
mikcnt <11929535+mikcnt@users.noreply.github.com>
2021-08-28 11:38:38 +0200

    Add script to generate dataset for the MDRNN training

--- src/pl_data/new_dataset.py
+++ src/pl_data/new_dataset.py
@@ -1,3 +1,4 @@
+import os
 from os.path import join
 import torch
 from tqdm import tqdm
@@ -7,19 +8,25 @@ from src.pl_data.CustomDataLoader import CustomDataLoader
 from src.pl_data.dataset import GameEpisodeDataset
 
 name = "."
-old_path = get_env("YOUR_VAL_DATASET_PATH")
-new_path = old_path.replace("val", "new_val")
+
+
+def generate_mdrnn_data(split_part):
+    assert split_part in ["train", "val", "test"]
+
+    old_path = get_env(f"YOUR_{split_part.upper()}_DATASET_PATH")
+    new_path = old_path.replace(split_part, "new_" + split_part)
+    os.makedirs(new_path, exist_ok=True)
+
+    dataset = GameEpisodeDataset(name=name, path=old_path)
+    loader = CustomDataLoader(dataset, batch_size=32, drop_last=True)
+
+    for i, batch in enumerate(tqdm(loader)):
+        batch = {k: v.squeeze() for k, v in batch.items()}
+        torch.save(batch, join(new_path, f"{i}.pt"))
 
 
 if __name__ == "__main__":
-    dataset = GameEpisodeDataset(name=name, path=old_path)
-
-    loader = CustomDataLoader(dataset, batch_size=32, drop_last=True)
-
-    batch = next(iter(loader))
-
-    count = 0
-    for i, batch in enumerate(tqdm(loader)):
-        count += 1
-        batch = {k: v.squeeze() for k, v in batch.items()}
-        torch.save(batch, join(new_path, f"{i}.pt"))
+    for split_part in ["train", "val", "test"]:
+        print(f"Generating the {split_part} MDRNN dataset...")
+        generate_mdrnn_data(split_part)
+        print("Done.")
